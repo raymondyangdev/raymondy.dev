7aaea05e3d14f8c56fd7fbab6b9bf7d6053ece9b
Raymond's MacBook Pro <raymond16@hotmail.co.nz>
2023-09-02 16:23:23 +1200

    Add skills/techstack section

---
+++ src/app/components/skills/TechStackData.tsx
@@ -0,0 +1,41 @@
+const TechStackData = {
+    Languages: [
+        {
+            src: './assets/images/techstack/javascript.svg',
+            alt: 'JavaScript Logo',
+        },
+        { src: './assets/images/techstack/java.svg', alt: 'Java Logo' },
+        { src: './assets/images/techstack/html5.svg', alt: 'HTML5 Logo' },
+        { src: './assets/images/techstack/css3.svg', alt: 'CSS3 Logo' },
+        {
+            src: './assets/images/techstack/typescript.svg',
+            alt: 'TypeScript Logo',
+        },
+        { src: './assets/images/techstack/python.svg', alt: 'Python Logo' },
+        { src: './assets/images/techstack/sqlite.svg', alt: 'SQLite Logo' },
+    ],
+
+    Technologies: [
+        { src: './assets/images/techstack/react.svg', alt: 'React Logo' },
+        { src: './assets/images/techstack/node.svg', alt: 'Node.js Logo' },
+        { src: './assets/images/techstack/nextjs.svg', alt: 'Next.js Logo' },
+        {
+            src: './assets/images/techstack/express.svg',
+            alt: 'Express.js Logo',
+        },
+        {
+            src: './assets/images/techstack/tailwind.svg',
+            alt: 'Tailwind.css Logo',
+        },
+        {
+            src: './assets/images/techstack/bootstrap.svg',
+            alt: 'Bootstrap Logo',
+        },
+    ],
+    Tools: [
+        { src: './assets/images/techstack/git.svg', alt: 'Git Logo' },
+        { src: './assets/images/techstack/apple.svg', alt: 'Apple Logo' },
+    ],
+};
+
+export default TechStackData;

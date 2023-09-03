c968119574adad0bf1c2e0a04e7610a651ca2e0f
Raymond's MacBook Pro <raymond16@hotmail.co.nz>
2023-09-04 10:34:59 +1200

    Add content to skills section

--- src/app/components/skills/TechStack.tsx
+++ src/app/components/skills/TechStack.tsx
@@ -50,15 +50,15 @@ export default function Skills() {
         <div className="flex flex-col">
             <PageHeading heading={'Skills'} size={'text-3xl'} margin={'my-6'} />
             <p className="font-josefinSans text-lg">
-                Praesent malesuada mi nisi, quis tempus magna venenatis ut.
-                Suspendisse tempor sem at scelerisque congue. Curabitur mollis,
-                mi id molestie finibus, metus sem sagittis nunc, euismod feugiat
-                orci neque non lorem. Aenean sollicitudin erat eu molestie
-                tempus. Nunc ac eros elementum, fermentum lacus at, pellentesque
-                mauris. Suspendisse eros neque, interdum euismod consectetur
-                convallis, porttitor at massa. Phasellus a ultrices arcu. Donec
-                viverra, lorem in pellentesque euismod, libero justo gravida
-                nibh, non ultricies justo purus ultrices est.
+                I have a preference for statically typed languages, appreciating
+                their capacity for robustness and predictability in software
+                development.
+            </p>
+            <p className="font-josefinSans text-lg">
+                Recently, I discovered a newfound passion for working with
+                React, particularly while building this website. Fuelled by its
+                component-based architecture, I am passionate about good
+                software design and writing clean, maintainable code.
             </p>
             <div className="flex justify-between">
                 <div className="mb-4 basis-72">

6b16d954fbf424057a2ac349dbfceeb55cf3f95f
Raymond's MacBook Pro <raymond16@hotmail.co.nz>
2023-09-04 13:38:57 +1200

    Optimise for mobile devices

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,22 +5,25 @@ export default function Footer() {
 
     return (
         <footer className="bg-slate-800 py-4 flex">
-            <div className="flex w-full justify-start gap-4 px-4 items-center">
-                <Link href={'https://github.com/raymondyangdev'}>
-                    <img
-                        className="w-8 h-8"
-                        src="./assets/images/socials/github.svg"
-                        alt="GitHub Logo"
-                    />
-                </Link>
-                <Link href={'https://www.linkedin.com/in/raymondyangdev/'}>
-                    <img
-                        className="w-8 h-8"
-                        src="./assets/images/socials/linkedin-in.svg"
-                        alt="LinkedIn Logo"
-                    />
-                </Link>
-                <div className="flex flex-1 gap-1 justify-center">
+            <div className="flex flex-col md:flex-row w-full justify-start gap-4 px-4 items-center">
+                <div className="flex gap-4">
+                    {' '}
+                    <Link href={'https://github.com/raymondyangdev'}>
+                        <img
+                            className="w-8 h-8"
+                            src="./assets/images/socials/github.svg"
+                            alt="GitHub Logo"
+                        />
+                    </Link>
+                    <Link href={'https://www.linkedin.com/in/raymondyangdev/'}>
+                        <img
+                            className="w-8 h-8"
+                            src="./assets/images/socials/linkedin-in.svg"
+                            alt="LinkedIn Logo"
+                        />
+                    </Link>
+                </div>
+                <div className="flex whitespace-nowrap flex-1 gap-1 justify-center">
                     Made with<span className="text-red-400">♥</span>by
                     <Link
                         className="hover:underline hover:decoration-sky-900 hover:underline-offset-4"
--- src/app/components/projects/Projects.tsx
+++ src/app/components/projects/Projects.tsx
@@ -22,7 +22,7 @@ export const TechStackColorMap: ITechStackColor = {
 export const ProjectsData = [
     {
         projectName: 'raymondyang.io',
-        description: `In less than a week, I taught myself React and Next.js via their documentation to create this website, getting it ready for the Summer of Tech's Meet & Greet event. I deployed it using Cloudflare Pages, and now, it has turned into a long-term passion project of mine 🙂`,
+        description: `In less than a week, I taught myself React and Next.js via their documentation to create this website in preparation for Summer of Tech's Meet & Greet event. I deployed it using Cloudflare Pages, and now, it has turned into a long-term passion project of mine 🙂`,
         link: 'https://github.com/raymondyangdev/raymondyang.io',
         projectImg: './assets/images/projects/raymondyang.io.png',
         techStack: ['React', 'TypeScript', 'Tailwind CSS', 'Next.js'],

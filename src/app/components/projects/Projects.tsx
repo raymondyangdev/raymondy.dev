00658e0580d8852734d2bc3da4738168e8f3fd82
Raymond's MacBook Pro <raymond16@hotmail.co.nz>
2024-05-27 12:42:01 +1200

    feat: Add Patrols project

--- src/app/components/projects/Projects.tsx
+++ src/app/components/projects/Projects.tsx
@@ -11,48 +11,65 @@ export type ITechStackColor = {
 };
 
 export const TechStackColorMap: ITechStackColor = {
-    React: 'bg-blue-400 text-blue-900',
-    TypeScript: 'bg-sky-500 text-sky-900',
-    'Tailwind CSS': 'bg-cyan-400 text-cyan-900',
-    'Next.js': 'bg-neutral-900 text-white',
-    JavaScript: 'bg-yellow-400 text-yellow-900',
-    'Express.js': 'bg-white text-neutral-900',
-    Java: 'bg-orange-400 text-orange-900',
-    SQLite: 'bg-cyan-200 text-cyan-900',
-    Svelte: 'bg-orange-500 text-orange-900',
-    'Stability.ai': 'bg-indigo-400 text-indigo-900',
+  React: "bg-blue-400 text-blue-900",
+  TypeScript: "bg-sky-500 text-sky-900",
+  "Tailwind CSS": "bg-cyan-400 text-cyan-900",
+  "Next.js": "bg-neutral-900 text-white",
+  JavaScript: "bg-yellow-400 text-yellow-900",
+  "Express.js": "bg-white text-neutral-900",
+  Java: "bg-orange-400 text-orange-900",
+  SQLite: "bg-cyan-200 text-cyan-900",
+  Svelte: "bg-orange-500 text-orange-900",
+  "Stability.ai": "bg-indigo-400 text-indigo-900",
+  Supabase: "bg-green-500 text-black",
+  PostgreSQL: "bg-cyan-200 text-cyan-900",
 };
 
 export const ProjectsData = [
-    {
-        projectName: 'raymondyang.io',
-        description: `I taught myself React and Next.js via the official documentation to create this website. Deployed it using Cloudflare Pages, and it has turned into a long-term passion project of mine 🙂`,
-        link: 'https://github.com/raymondyangdev/raymondyang.io',
-        projectImg: './assets/images/memoji.png',
-        techStack: ['React', 'TypeScript', 'Tailwind CSS', 'Next.js'],
-    },
-    {
-        projectName: 'The Art Thief',
-        description: `Our team achieved 1st place at WDCC x SESA Hackathon for The Art Thief - a web-based game developed in 24 hours that utlises Stability.ai's Stable Diffusion API to draw replicas of renowned historical masterpieces from simple line sketches and text prompts.`,
-        link: 'https://github.com/raymondyangdev/the-art-thief-wdccxsesa',
-        projectImg: './assets/images/projects/the-art-thief.svg',
-        techStack: ['Svelte', 'Stability.ai', 'Express.js'],
-    },
-    {
-        projectName: 'Bulls and Cows',
-        description: `A console-based game developed for a COMPSCI 718 assignment to demonstrate the application of fundamental object-oriented programming principles. This project features multiple levels of AI difficulty, including easy, medium, and hard. Notably, the Hard AI employs a minimax algorithm to optimise its guessing strategy against the player.`,
-        link: 'https://github.com/raymondyangdev/bulls-and-cows',
-        projectImg: './assets/images/projects/bulls-and-cows.svg',
-        techStack: ['Java'],
-    },
-    {
-        projectName: 'Fluffy Frogs',
-        description: `A full-stack blog app with CRUD features created for the final assignment of COMPSCI 719 as a team of 4. This application includes essential CRUD features. Our initial front-end design was not completed by the deadline, so I integrated Tailwind CSS to assist with polishing the front-end. I enjoyed working with SQL and designing the database. The ER diagram can be found in the root directory of the GitHub repository.
+  {
+    projectName: "Patrols",
+    description: `Currently developing a progressive web app to enhance incident reporting procedures for Community Patrol New Zealand, within a team of 11 developers and designers utilising React, Node, Express, Supabase, PostgreSQL, and Prisma, while
+following Agile methodologies.`,
+    link: "https://github.com/UoaWDCC/patrols",
+    projectImg: "./assets/images/projects/cpnz.png",
+    techStack: [
+      "React",
+      "TypeScript",
+      "Tailwind CSS",
+      "Express.js",
+      "Supabase",
+      "PostgreSQL",
+    ],
+  },
+  {
+    projectName: "The Art Thief",
+    description: `Our team achieved 1st place at WDCC x SESA Hackathon for The Art Thief - a web-based game developed in 24 hours that utlises Stability.ai's Stable Diffusion API to draw replicas of renowned historical masterpieces from simple line sketches and text prompts.`,
+    link: "https://github.com/raymondyangdev/the-art-thief-wdccxsesa",
+    projectImg: "./assets/images/projects/the-art-thief.svg",
+    techStack: ["Svelte", "Stability.ai", "Express.js"],
+  },
+  {
+    projectName: "raymondyang.io",
+    description: `I taught myself React and Next.js via the official documentation to create this website. Deployed it using Cloudflare Pages`,
+    link: "https://github.com/raymondyangdev/raymondyang.io",
+    projectImg: "./assets/images/memoji.png",
+    techStack: ["React", "TypeScript", "Tailwind CSS", "Next.js"],
+  },
+  {
+    projectName: "Bulls and Cows",
+    description: `A console-based game developed for a COMPSCI 718 assignment to demonstrate the application of fundamental object-oriented programming principles. This project features multiple levels of AI difficulty, including easy, medium, and hard. Notably, the Hard AI employs a minimax algorithm to optimise its guessing strategy against the player.`,
+    link: "https://github.com/raymondyangdev/bulls-and-cows",
+    projectImg: "./assets/images/projects/bulls-and-cows.svg",
+    techStack: ["Java"],
+  },
+  {
+    projectName: "Fluffy Frogs",
+    description: `A full-stack blog app with CRUD features created for the final assignment of COMPSCI 719 as a team of 4. This application includes essential CRUD features. Our initial front-end design was not completed by the deadline, so I integrated Tailwind CSS to assist with polishing the front-end. I enjoyed working with SQL and designing the database. The ER diagram can be found in the root directory of the GitHub repository.
 `,
-        link: 'https://github.com/raymondyangdev/fluffy-frogs-blog',
-        projectImg: './assets/images/projects/fluffy-frog.svg',
-        techStack: ['JavaScript', 'Tailwind CSS', 'Express.js', 'SQLite'],
-    },
+    link: "https://github.com/raymondyangdev/fluffy-frogs-blog",
+    projectImg: "./assets/images/projects/fluffy-frog.svg",
+    techStack: ["JavaScript", "Tailwind CSS", "Express.js", "SQLite"],
+  },
 ];
 
 export default function Projects() {
